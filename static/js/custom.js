f48ad08589d4833c163da26ec1e4b06120b13e9e
maodaisuki <nyanyanyapu@gmail.com>
2024-03-14 13:39:21 +0800

    Update

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,7 +1,6 @@
 function switchPulldownMenu() {
     var pulldownMenu = document.getElementsByClassName('pulldown-menu')[0];
     var pulldownMenuToggle = document.getElementsByClassName('pulldown-menu-toggle')[0];
-    var dom;
     if(pulldownMenu.classList.contains('show')) {
         pulldownMenu.classList.remove('show');
         document.getElementsByClassName('main')[0].classList.remove('blur');
@@ -16,5 +15,6 @@ function switchPulldownMenu() {
         document.getElementsByClassName('footer')[0].classList.add('blur');
         document.getElementById('top-link').classList.add('blur');
         pulldownMenuToggle.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>'
+
     }
 }
