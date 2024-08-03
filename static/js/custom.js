51212e83133fd436146e36173b99c134bbee5e45
maodaisuki <nyanyanyapu@gmail.com>
2024-08-03 18:40:46 +0800

    添加图片放大

--- static/js/custom.js
+++ static/js/custom.js
@@ -18,3 +18,13 @@ function switchPulldownMenu() {
 
     }
 }
+
+// zoom.js 添加 data-action
+function imgAddDataAction()
+{
+    const imglist = document.getElementsByTagName("img")
+    for(i of imglist)
+    {
+        i.setAttribute("data-action", "zoom");
+    }
+}
